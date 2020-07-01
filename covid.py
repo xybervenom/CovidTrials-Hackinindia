ca7d3c6713c0bb9709f99ba05d655083f14271d1
Jayant Jain <40491811+xybervenom@users.noreply.github.com>
2020-07-01 23:04:14 +0530

    Add files via upload

--- covid.py
+++ covid.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import numpy as np
 # pandas defaults
 pd.options.display.max_columns = 500
 pd.options.display.max_rows = 500
+
+#Reading and splitting
 df=pd.read_excel("covid.xlsx")
-df.isnull().sum().max()
+X = df.drop(labels='result', axis=1) # Features
+y = df.loc[:,'result']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1,stratify=y)
+
+#Model
 from sklearn import svm
 clf= svm.SVC(kernel='linear')
 clf.fit(X_train,y_train)
